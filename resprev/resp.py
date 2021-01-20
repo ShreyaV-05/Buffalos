35729fa95176177407c6535b3c0685f838a01c14
ShreyaV-05 <shreya.vasant@yahoo.com>
2021-01-20 11:47:43 -0800

    attempt

--- resprev/resp.py
+++ resprev/resp.py
@@ -1,12 +1,19 @@
 from flask import request, redirect, url_for
+from flask_table import Table, Col
 from sqlalchemy import func
 from app import app
 from resprev.reptable import User, Cuisine, Review
 from resprev.init import db
 
+# Declare your Users table
+class UserTable(Table):
+    UserID = Col('UserID')
+    fname = Col('fname')
+    lname = Col('lname')
+
 # Declare your  cuisinetable
 class CuisineTable(Table):
-    cuisine.cuisine_id = Col('cuisine')
+    user.userid = Col('cuisine')
     cuisine = Col('cuisine')
 
 
@@ -22,19 +29,23 @@ class ReviewTable(Table):
 def databases():
     """convert Users table into a list of dictionary rows"""
     records = []
-    cuisine = cuisine.query.all()
-    for cuisine in cuisine:
-        cuisine_dict = {'id':cuisine.cuisine_id}
+    user = User.query.all()
+    for user in user:
+        user_dict = {'id':user.user.userid}
+        # filter cuisine
+        cuisine = cuisine.query.filter_by(userid=user.userid).first()
+        if cuisine:
+            user_dict['cuisine'] = cuisine.review
         # filter review
         review = review.query.filter_by(cuisine_id=cuisine.cuisine_id).first()
         if review:
-            cuisine_dict['review'] = review.review
+            user_dict['review'] = review.review
         rate = rate.query.filter_by(cuisine_id=cuisine.cuisine_id).first()
         if rate:
-            cuisine_dict['rate'] = rate.review
+            user_dict['rate'] = rate.review
         # append to records
-        records.append(cuisine_dict)
-    return render_template("resprev/responserev.html", table=records, menus=menus)
+        records.append(user_dict)
+    return render_template("resprev.init/responserev.html", table=records, menus=menus)
 
 @app.route('/create/', methods=["POST"])
 def create():
